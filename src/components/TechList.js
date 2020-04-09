3bc0f0a0ec3c5df9874282551e24e1f31e7ed492
CesarAugustoMor <cesaramfurst@gmail.com>
2020-04-09 14:15:50 -0300

    Mudança de estados

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
 
 export class TechList extends Component {
-  state = { teches: ['Node.js', 'ReactJs', 'React Naive'] };
-  render() {
-    console.log(this.state);
+  state = { newTech: '', teches: ['Node.js', 'ReactJs', 'React Naive'] };
 
+  handleInputChange = (e) => {
+    this.setState({ newTech: e.target.value });
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    this.setState({
+      teches: [...this.state.teches, this.state.newTech],
+      newTech: '',
+    });
+  };
+  render() {
     return (
-      <ul>
-        <li>Node.js</li>
-        <li>ReactJs</li>
-        <li>React Native</li>
-      </ul>
+      <form onSubmit={this.handleSubmit}>
+        <ul>
+          {this.state.teches.map((tech) => (
+            <li key={tech}>{tech}</li>
+          ))}
+        </ul>
+        <input
+          type="text"
+          onChange={this.handleInputChange}
+          value={this.state.newTech}
+        />
+        <button type="submit">Enviar</button>
+      </form>
     );
   }
 }
